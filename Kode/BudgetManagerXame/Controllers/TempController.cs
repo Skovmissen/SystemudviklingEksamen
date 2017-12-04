7ad35ac807a76e1ce8f979faf845562f0726d2b0
Lasse Rüss Meldgaard <Lassemeldgaard@hotmail.dk>
2017-12-04 09:51:44 +0100

    Delete påbegyndt

---
+++ Kode/BudgetManagerXame/Controllers/TempController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BudgetManagerXame.Models;
+using BudgetManagerXame.Classes;
+
+namespace BudgetManagerXame.Controllers
+{
+    public class TempController : Controller
+    {
+        // GET: Temp
+        public ActionResult Delete(Budget budget)
+        {
+            
+            return View();
+        }
+    }
+}
