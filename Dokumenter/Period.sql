1f7f35b27fc6f08646b7d65df78154c475451877
Anders Oxenvad <Anders.Oxenvad@gmail.com>
2017-12-06 12:45:19 +0100

    sql period

---
+++ Dokumenter/Period.sql
@@ -0,0 +1,17 @@
+USE [BudgetManagerExam]
+GO
+
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Januar')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Febuar')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Marts')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'April')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Maj')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Juni')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Juli')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'August')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'September')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'Oktober')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'November')
+INSERT INTO [dbo].[Period] ([Name]) VALUES (N'December')
+
+
